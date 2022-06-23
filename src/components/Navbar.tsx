7161173f52db2d025dd9f1758962578a2aca0c1f
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-06-23 16:26:25 +0100

    Navbar and navlinks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,22 @@
+import { Container, Nav, Navbar as NavbarBs } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
 export function Navbar() {
   return (
-    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-      <a className='navbar-brand' href='#'>
-        Navbar
-      </a>
-      <button
-        className='navbar-toggler'
-        type='button'
-        data-toggle='collapse'
-        data-target='#navbarSupportedContent'
-        aria-controls='navbarSupportedContent'
-        aria-expanded='false'
-        aria-label='Toggle navigation'
-      >
-        <span className='navbar-toggler-icon'></span>
-      </button>
-
-      <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-        <ul className='navbar-nav mr-auto'>
-          <li className='nav-item active'>
-            <a className='nav-link' href='#'>
-              Home <span className='sr-only'>(current)</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <NavbarBs className='bg-white shadow-sm mb-3'>
+      <Container>
+        <Nav>
+          <Nav.Link as={NavLink} to='/'>
+            Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to='/store'>
+            Store
+          </Nav.Link>
+          <Nav.Link as={NavLink} to='/about'>
+            About
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </NavbarBs>
   );
 }
