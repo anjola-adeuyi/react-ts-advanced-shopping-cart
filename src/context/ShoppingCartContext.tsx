b9ce2b0462f67478a2411c0e9cc3ea8cde0bb3bc
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-06-23 17:24:14 +0100

    shopping cart context and provider

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,19 @@
+import { createContext, ReactNode, useContext } from 'react';
+
+interface ShoppingCartProviderProps {
+  children: ReactNode;
+}
+
+const ShoppingCartContext = createContext({});
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext);
+}
+
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  return (
+    <ShoppingCartContext.Provider value={{}}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+}
