9cc178e3dd5fbe661f9af167410a97473d9e4e7e
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-06-23 16:20:03 +0100

    Navbar setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import { Home, About, Store } from './pages';
+import { Navbar } from './components';
 
 function App() {
   return (
-    <Container>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/store' element={<Store />} />
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container className='mb-4'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/store' element={<Store />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
