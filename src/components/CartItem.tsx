c5434cd8f0b54175516246ac32088dbe9ba89f7f
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-06-23 18:38:45 +0100

    completed cartItem rendering list fn

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,8 +1,42 @@
+import { Button, Stack } from 'react-bootstrap';
+import { useShoppingCart } from '../context/ShoppingCartContext';
+import StoreItems from '../data/items.json';
+import { formatCurrency } from '../utils/formatCurrency';
+
 interface CartItemProps {
   id: number;
   quantity: number;
 }
 
 export function CartItem({ id, quantity }: CartItemProps) {
-  return <h1>item 1</h1>;
+  const { removeItem } = useShoppingCart();
+  const item = StoreItems.find((item) => item.id === id);
+  if (item == null) return null;
+
+  return (
+    <Stack direction='horizontal' gap={2} className='d-flex align-items-center'>
+      <img
+        src={item.imgUrl}
+        alt={item.name}
+        style={{ width: '125px', height: '75px', objectFit: 'cover' }}
+      />
+      <div className='me-auto'>
+        <div>
+          {item.name}{' '}
+          {quantity > 1 && (
+            <span className='text-muted' style={{ fontSize: '.65rem' }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+        <div className='text-muted' style={{ fontSize: '.75rem' }}>
+          {formatCurrency(item.price)}
+        </div>
+      </div>
+      <div>{formatCurrency(item.price * quantity)}</div>
+      <Button variant='outline-danger' size='sm' onClick={() => removeItem(id)}>
+        &times;
+      </Button>
+    </Stack>
+  );
 }
